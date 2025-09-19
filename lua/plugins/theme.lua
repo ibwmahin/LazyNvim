211e46bf2af6cef743783a52d59294b9a7830e36
ibwmahin <ibwmahin@gmail.com>
2025-09-19 20:28:51 +0600

    plugins

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -16,28 +16,28 @@ return {
   -- },
 
   -----------tookyonight color scheme 🌌
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     style = "storm", -- options: "storm", "moon", "night", "day"
-  --     transparent = false, -- << Enable transparency
-  --     terminal_colors = true,
-  --     styles = {
-  --       comments = { italic = true },
-  --       keywords = { italic = true },
-  --       functions = {},
-  --       variables = { italic = true },
-  --     },
-  --     sidebars = { "qf", "help", "terminal", "lazy", "lspinfo", "packer", "spectre_panel", "toggleterm" },
-  --     dim_inactive = true,
-  --   },
-  --   config = function(_, opts)
-  --     require("tokyonight").setup(opts)
-  --     vim.cmd([[colorscheme tokyonight]])
-  --   end,
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "moon", -- options: "storm", "moon", "night", "day"
+      transparent = true, -- << Enable transparency
+      terminal_colors = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = { italic = true },
+      },
+      sidebars = { "qf", "help", "terminal", "lazy", "lspinfo", "packer", "spectre_panel", "toggleterm" },
+      dim_inactive = true,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 
   ------------------- OXOCAEBON;
   -- {
@@ -60,22 +60,22 @@ return {
   -- },
 
   -------------------------GITHUB THEME
-  {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("github-theme").setup({
-        --  ...
-      })
-
-      -- vim.cmd("colorscheme github_dark")
-      vim.cmd("colorscheme github_dark_default")
-      -- vim.cmd("colorscheme github_dark_dimmed")
-      -- vim.cmd("colorscheme github_dark_high_contrast")
-      -- vim.cmd("colorscheme github_dark_colorblind")
-      -- vim.cmd("colorscheme github_dark_tritanopia")
-    end,
-  },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   name = "github-theme",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     require("github-theme").setup({
+  --       --  ...
+  --     })
+  --
+  --     -- vim.cmd("colorscheme github_dark")
+  --     vim.cmd("colorscheme github_dark_default")
+  --     -- vim.cmd("colorscheme github_dark_dimmed")
+  --     -- vim.cmd("colorscheme github_dark_high_contrast")
+  --     -- vim.cmd("colorscheme github_dark_colorblind")
+  --     -- vim.cmd("colorscheme github_dark_tritanopia")
+  --   end,
+  -- },
 }
